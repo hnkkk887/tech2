69c8e4a03e7f0fbbb4220056a9982853111ff6ee
hnkkk887 <127996111+hnkkk887@users.noreply.github.com>
2023-03-18 02:32:10 +0100

    Update TextTwo.js

--- src/components/TextWithImage/TextTwo.js
+++ src/components/TextWithImage/TextTwo.js
@@ -1,13 +1,32 @@
+import { useState } from 'react';
+import Web3 from 'web3';
+import ReactPlayer from 'react-player';
 import { Container, Row, Col } from "react-bootstrap";
 
 const TextWithImageOne = () => {
+  const [address, setAddress] = useState('');
+
+  async function connectMetamask() {
+    try {
+      // Request access to the user's accounts
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      setAddress(accounts[0]);
+
+      // Create a Web3 instance using the user's provider
+      const web3 = new Web3(window.ethereum);
+      console.log('Web3 instance:', web3);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div className="text-image-section space-pb--r100">
       <Container>
         <Row className="align-items-center">
           <Col>
             <div className="heading-s1 space-mb--20">
-              <h2>What is ETH?</h2>
+              <h2>What is Ethereum?</h2>
             </div>
             <p>
               ETH, or Ethereum, is a popular cryptocurrency that offers a number of advantages to buyers. Firstly, using ETH for purchases can provide an extra layer of security, as transactions are recorded on a decentralized public ledger that is nearly impossible to tamper with. Additionally, because Ethereum is not controlled by any central authority, transactions can be processed quickly and with low fees.
@@ -29,14 +48,15 @@ const TextWithImageOne = () => {
         <Row className="align-items-center">
           <Col>
             <div className="heading-s1 space-mb--20">
-              <h2>How to use ETH?</h2>
+              <h2>How to use Ethereum?</h2>
             </div>
             <p>
               Using MetaMask with ETH enables you to securely store and manage your cryptocurrency, as well as easily connect to web apps and interact with the Ethereum blockchain.
             </p>
             <p>
-              You can check this youtube tutorial on how to setup your MetaMask wallet: <a href="https://www.youtube.com/watch?v=afATAw7iuUM">https://www.youtube.com/watch?v=afATAw7iuUM</a>
+              You can check this youtube tutorial on how to setup your MetaMask wallet:
             </p>          
+            <ReactPlayer width="100%" url='https://www.youtube.com/watch?v=Huc9CcPuGn8' />
           </Col>        
         </Row>
       </Container>
@@ -45,14 +65,15 @@ const TextWithImageOne = () => {
         <Row className="align-items-center">
           <Col>
             <div className="heading-s1 space-mb--20">
-              <h2>How to buy ETH?</h2>
+              <h2>How to buy Ethereum?</h2>
             </div>
             <p>
               Buying cryptocurrency with MetaMask has never been easier. Just head on over to the "Buy" button to get started. 
             </p>
             <p>
-              You can check this youtube tutorial on how to buy eth using your credit card in the metamask wallet: <a href="https://www.youtube.com/watch?v=afATAw7iuUM"> https://www.youtube.com/watch?v=afATAw7iuUM </a>            
-            </p>            
+              You can check this youtube tutorial on how to buy eth using your credit card in the metamask wallet:            
+            </p>     
+            <ReactPlayer width="100%" url='https://www.youtube.com/watch?v=_hYKyOvXa2s&t=28s' />       
           </Col>        
         </Row>
       </Container>
